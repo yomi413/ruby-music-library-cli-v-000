9af502305a3667c09d893cb2b9f3a49c69a7a830
Yomaira Escano <yjescano@gmail.com>
2017-10-13 14:17:57 +0000

    automatically backed up by learn

---
+++ lib/009_music_importer.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class MusicImporter
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir[@path+"/*.mp3"].collect do |file|
+      file.split("/").last
+      # binding.pry
+    end
+  end
+
+  def import
+    files.each do |file|
+      Song.create_from_filename(file)
+    end
+  end
+
+end
